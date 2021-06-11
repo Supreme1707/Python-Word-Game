ae6006e0aec676f67ef07dddd83904100a10eb3f
Supreme Coder <highlyintelligentbeing1707@gmail.com>
2021-06-11 19:06:30 +0530

    Added difficulty and a 10 second timeout

--- __init__.py
+++ __init__.py
@@ -2,11 +2,11 @@ from game import *
 
 
 def main_method():
-    start_obj = Start()
+    start_obj = Game()
+    start_obj.difficulty()
     start_obj.intro()
     start_obj.dict_select()
-    game_obj = Game()
-    game_obj.game_method()
+    start_obj.game_method()
 
 
 main_method()
--- game.py
+++ game.py
@@ -7,19 +7,50 @@ import string
 output_opt = True
 
 
-class Start:
+class Game:
+
+    def __init__(self):
+        self.difficulty_mode = 'medium'
+        self.score = 0
+        self.string_list = list(string.ascii_lowercase)
+
+    def difficulty(self):
+        print(colored("Please pick a difficulty with the number: ", "magenta"), end="")
+        print(colored("1 easy, ", "green"), end="")
+        print(colored("2 medium ", "blue"), end="")
+        print(colored("or ", "magenta"), end="")
+        print(colored("3 hard ", "red"))
+        mode = input()
+        try:
+            if int(mode) == 1:
+                self.difficulty_mode = 'easy'
+                self.string_list.remove("q")
+                self.string_list.remove("w")
+                self.string_list.remove("x")
+                self.string_list.remove("y")
+                self.string_list.remove("z")
+            elif int(mode) == 2:
+                self.difficulty_mode = 'medium'
+                self.string_list.remove("x")
+                self.string_list.remove("y")
+                self.string_list.remove("z")
+            elif int(mode) == 3:
+                self.difficulty_mode = 'hard'
+        except:
+            print("There was an error. It may be because you input a string instead of a number! Please input again.")
+            self.difficulty()
 
     def intro(self):
         start_string = "Hello there! Welcome to the Word Game. Here, you will be given a letter and a single digit " \
                        "number.\nYou have to input a word from the selected English Dictionary with the number of " \
-                       "characters being the digit given.\n"
+                       f"characters being the digit given.\n"
 
         # print(start_string)
         print_(start_string, 'yellow')
 
     def dict_select(self):
         global selected_dict
-        english_dictionary = "Enter 1 for US dictionary and 2 for UK dictionary."
+        english_dictionary = "Enter 1 for US dictionary and 2 for UK dictionary:"
 
         # print(english_dictionary)
         print_(english_dictionary, 'blue')
@@ -36,14 +67,13 @@ class Start:
             print_("Wrong input.", 'red')
             self.dict_select()
 
-
-class Game:
-    def __init__(self):
-        self.score = 0
-        self.string_list = list(string.ascii_lowercase)
-
     def game_method(self):
-        counter = 20
+        if self.difficulty_mode == 'easy':
+            counter = 10
+        elif self.difficulty_mode == 'medium':
+            counter = 15
+        elif self.difficulty_mode == 'hard':
+            counter = 20
         i = 0
         while i < counter:
             global rand_num
@@ -55,6 +85,8 @@ class Game:
             user_ans = input()
             self.score_method(selected_dict.check(user_ans))
             i += 1
+            print_("Next question coming in 10 seconds...\n", "blue")
+            sleep(10)
 
         self.check_score()
 
@@ -62,15 +94,15 @@ class Game:
         if user_ans.startswith(rand_alphabet):
             if len(user_ans) == rand_num:
                 if check:
-                    print_("Correct! 1 point added!\n", 'green')
+                    print_("Correct! 1 point added!", 'green')
                     self.score += 1
                 else:
-                    print_("Uh oh! Your word wasn't in the dictionary...\n", 'red')
+                    print_("Uh oh! Your word wasn't in the dictionary!\n", 'red')
             else:
                 print_(f"Uh oh! The number of characters in your word wasn't {rand_num}!\n",
-                            'red')
+                       'red')
         else:
-            print_(f"Uh oh! Your word did not start with {rand_alphabet}\n", 'red')
+            print_(f"Uh oh! Your word did not start with {rand_alphabet}!\n", 'red')
 
     def check_score(self):
         print_(f"Your score was {self.score}. End of Game.", 'green')
